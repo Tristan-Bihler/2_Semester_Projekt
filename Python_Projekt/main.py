e2ec30a678bae99b2e715570334dc322757e44d2
Tristan Bihler <tristanbihler@outlook.com>
2025-05-20 13:33:14 +0200

    Projekt

---
+++ Python_Projekt/main.py
@@ -0,0 +1,22 @@
+"""
+Projekt:        
+Ersteller:       
+Version:        0.0.1    
+
+Über das Projekt:
+    
+"""
+
+def input():
+    pass
+
+def verarbeitung():
+    pass
+
+def output():
+    pass
+
+if __name__ == "__main__":
+    input()
+    verarbeitung()
+    output()
