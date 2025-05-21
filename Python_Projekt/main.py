909d42dd2ffdf7fe4bf59c86d53d15a4daeda3ef
Tristan Bihler <tristanbihler@outlook.com>
2025-05-22 00:07:20 +0200

    Wertigkeitssystem

--- Python_Projekt/main.py
+++ Python_Projekt/main.py
@@ -1,5 +1,5 @@
 """
-Projekt:        Empfegulngsystem     
+Projekt:        Empfehlungsystem     
 Ersteller:      Halter Carina, Mirwald Laura, Finé Semian, Bihler Tristan
 Version:        0.0.1    
 
@@ -7,19 +7,39 @@ Version:        0.0.1
     
 """
 import tkinter as tk
+from tkinter import ttk
+import File_handling as fh
 
-window = tk.root()
+momentarer_Warenkorb = {}
+Wertigkeit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-def input():
-    pass
+def Warenkorb(Produkt_liste, menge):
+    Produkt = combo.get()
+    Produkt = Produkt.split(" ")
+    Produkt = Produkt[0]
+    try:
+        momentarer_Warenkorb[Produkt] = (Produkt_liste[Produkt][0], momentarer_Warenkorb[Produkt][1] + menge)
+        
+    except:
+        momentarer_Warenkorb[Produkt] = (Produkt_liste[Produkt][0], menge)
+    
+    for i in range(len(Produkt_liste[Produkt][1])):
+            Wertigkeit[i] = int(Wertigkeit[i]) + int(Produkt_liste[Produkt][1][i])
+    
+    print(Wertigkeit)
+    print(momentarer_Warenkorb)
 
-def verarbeitung():
-    pass
 
-def output():
-    pass
 
 if __name__ == "__main__":
-    input()
-    verarbeitung()
-    output()
+    Produkte = fh.load(r"Python_Projekt\Produkt_list.txt")
+    Auswahl = []
+    
+    for i in Produkte:
+        Auswahl.append((i, Produkte[i][0]))
+    
+    window = tk.Tk()
+    combo = ttk.Combobox(window, values = Auswahl)
+    combo.pack()
+    button = ttk.Button(text="Display selection", command = lambda: Warenkorb(Produkte ,1)).pack()
+    window.mainloop()
