45f8029aaa85fb933141715e283f9cef863c4306
Tristan Bihler <tristanbihler@outlook.com>
2025-06-17 08:00:52 +0200

    Kommentierung

--- Python_Projekt/Ohne_Firebase/main.py
+++ Python_Projekt/Ohne_Firebase/main.py
@@ -12,7 +12,15 @@ Dies beinhaltet die Konzeption und Implementierung einer geeigneten Datenstruktu
 Wir haben uns für die konkrete Entwicklung eines Empfehlungssystems für einen Streaming-Dienst entschieden. Hierfür muss zum Beispiel ein Weg gefunden werden, um herauszufinden welche Filme einem ähnlichen Genre angehören und ob diese dem Nutzer gefallen können oder eher nicht. 
 Außerdem soll es eine Möglichkeit geben, dass Filme in einer Favoritenliste gespeichert werden könne und zusätzlich darauf Empfehlungen angezeigt werden.
 
-Wichtig, das Projekt muss aus dem Ordner Abgaben geöffnet werden, und nicht aus einem andern. Sonst kann der Compiler die Pfade der Datenbanke nicht finden.
+Wichtig, das Projekt muss aus dem Ordner Abgaben geöffnet werden, und nicht aus einem anderen. Sonst kann der Compiler die Pfade der Datenbanken nicht finden.
+
+In der Login-Übersicht kann sich der User unter „Login“ anmelden und unter „Registrieren“ einen Nutzer anlegen. Nach dem erfolgreichen Einloggen wird der Nutzer auf die Übersichtsseite weitergeleitet. Auf dieser kann der Nutzer auf der linken Seite die Filme sehen.
+
+Nun kann der Nutzer einen Film auswählen. Die Applikation zeigt dann die Details zu diesem Film auf der unteren Seite der generellen Übersicht an. Hier kann der Film zur Favoritenliste hinzugefügt werden. Auf der rechten Seite aktualisieren sich dann die Filmempfehlungen basierend auf der eigenen Favoritenliste.
+
+Nun kann der Nutzer oben rechts zur Favoritenseite wechseln und die Favoritenliste einsehen und gegebenfalls einzelne Filme entfernen. Zudem werden auf der rechten Seite Filme von ähnlichen Nutzerprofilen vorgeschlagen, welche wie gewohnt zur Favoritenliste hinzugefügt werden können.
+
+Das Ausloggen erfolgt nach dem Beenden der Applikation.
 """
 
 if __name__ == "__main__":
